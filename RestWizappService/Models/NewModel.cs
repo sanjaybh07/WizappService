8327f5a112f1f31ce47c3ce78df9d4d8be099bd5
sanjaybh07 <38379650+sanjaybh07@users.noreply.github.com>
2025-01-12 18:46:16 +0530

    Update NewModel.cs

--- RestWizappService/Models/NewModel.cs
+++ RestWizappService/Models/NewModel.cs
@@ -6,9 +6,9 @@ namespace RestWizappService.Models;
 public class NewModel1
 {
 //Changed the model class name
-  public int MyProperty { get; set; }
+  public int MyProperty1 { get; set; }
   public int MyProperty3 { get; set; }
 
-  public int MyProperty { get; set; }
+  public int MyProperty2 { get; set; }
 
 }
